83d819540880753ff4b6adce142d1dd56faeb3bc
matsu0314 <19185103+matsu0314@users.noreply.github.com>
2022-01-09 09:52:49 +0900

    add router

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,7 +1,14 @@
+import { useLocation, useHistory } from "react-router-dom";
+const history = useHistory();
+const onClickBack = () => history.goBack();
+
 export const Page1DetailA = () => {
+  const { state: location } = useLocation();
+  console.log(location);
   return (
     <div>
       <h1>Page1DetailAです。</h1>
+      <button onClick={onClickBack}>戻る</button>
     </div>
   );
 };
